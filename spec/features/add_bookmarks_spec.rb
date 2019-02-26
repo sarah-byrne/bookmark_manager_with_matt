180fc0f1136b150606bc58197724ae13fab87e83
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-26 16:46:14 +0000

    failing feature test for showing bookmark link after adding a bookmark

--- spec/features/add_bookmarks_spec.rb
+++ spec/features/add_bookmarks_spec.rb
@@ -5,7 +5,7 @@ feature 'Adding bookmarks' do
     fill_in 'url', with: 'http://www.hotmail.com'
     fill_in 'title', with: 'email account'
     click_button 'add to manager'
-    expect(page).to have_content('http://www.hotmail.com')
-    expect(page).to have_content('http://www.facebook.com')
+    expect(page).to have_link('email account', href: 'http://www.hotmail.com')
+    expect(page).to have_link('social media fave', href: 'http://www.facebook.com')
   end
 end
