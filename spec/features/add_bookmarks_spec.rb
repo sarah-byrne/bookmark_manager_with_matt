b2e097d9ef68603556502b16a6b514e81b82528b
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-26 16:39:50 +0000

    update add bookmark feature test to fill in title field

--- spec/features/add_bookmarks_spec.rb
+++ spec/features/add_bookmarks_spec.rb
@@ -3,6 +3,7 @@ feature 'Adding bookmarks' do
     visit '/'
     click_button 'add bookmark'
     fill_in 'url', with: 'http://www.hotmail.com'
+    fill_in 'title', with: 'email account'
     click_button 'add to manager'
     expect(page).to have_content('http://www.hotmail.com')
     expect(page).to have_content('http://www.facebook.com')
