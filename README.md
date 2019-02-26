a98a6cf2c861f7f188ed17afabe99f75e5e9bcca
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-26 15:59:37 +0000

    failing feature test for adding bookmark

--- README.md
+++ README.md
@@ -6,7 +6,12 @@
 As a user
 So I can view all of my bookmarks
 I want to be able to view a list of bookmarks on a page
+
+As a user
+So I can easily revisit websites I like
+I want to be able to add a bookmark to the bookmark manager 
 `
+
 
 # Using the databases
 
---
+++ spec/features/add_bookmarks_spec.rb
@@ -0,0 +1,9 @@
+feature 'Adding bookmarks' do
+  scenario 'add a single bookmark' do
+    visit '/'
+    click_button 'add bookmark'
+    fill_in 'url', with: 'http://www.hotmail.com'
+    click_button 'add to manager'
+    expect(page).to have_content('http://www.hotmail.com')
+  end
+end
